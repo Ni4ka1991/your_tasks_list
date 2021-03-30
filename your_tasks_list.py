c3845f95079577b8b1309c9670bfd5f783ab49b9
Ni4ka <astahova_nica@mail.ru>
2021-03-30 12:22:33 +0300

    Add file your_tasks_list.py

--- your_tasks_list.py
+++ your_tasks_list.py
@@ -19,33 +19,86 @@ tasks = [
 
 # ######################
 
+# ##### USER MAN  ##########
+
+system( "clear" )
+print()
+print( "Info how to use TODO list" )
+print()
+print( "***************************************" )
+print()
+print( "Enter 1 to add a new task in TODO list" )
+print( "Enter 2 to show TODO list" )
+print( "Enter 3 to remove one task in TODO list" )
+print( "Enter 4 to clear TODO list" )
+print( "Enter 0 to EXIT" )
+print()
+
+# ########################
+
+
+
+
 while True:
     
     # ######## interaction ######
     
-    new_task = input( " Add task: \n" )
+ new_task = input( " Enter any number from the menu: \n" )
 
-    if( new_task == "\n" ):
-        break
+# ### -0- ####
+
+ if( new_task == "0" ):
+  break
+
+# ############
+
+# ### -1- #### 
+
+ elif( new_task == "1" ):
+  
+  new_task = input( " Add new task: \n" )
+  
+  if new_task not in tasks:
+   tasks.append( new_task)
+  
+   print( "Your new TODO list:", len( tasks ), ":-)" )
+   for i in range( len( tasks )):
+    print( "\t", i + 1, ">", tasks[i] )
+
+  else:
+    print( "This task already exists!" )
+
+
+# ### -2- ####
+
     
-    if new_task not in tasks:
-        tasks.append( new_task)
-    else:
-        print( "This task already exists!" )
-    
+ elif( new_task == "2" ):
+  system( "clear" )
+  print()
+  print( "Your actual TODO list:", len( tasks ), ":-)" )
+  for i in range( len( tasks )):
+   print( "\t", i + 1, ">", tasks[i] )
+
+# ### -3- ####
+
+ elif( new_task == "3" ):
+  
+  system( "clear" )
+  print()
+  print( "Your actual TODO list:", len( tasks ), ":-)" )
+  for i in range( len( tasks )):
+   print( "\t", i + 1, ">", tasks[i] )
+  
+  i = input( " What task do you want to remove: \n" )
+  if( 0 <= i <= len(tasks )):
+   
+
+
+
+
     # ######## interaction ######
 
     
-    # ## print the tasks #######
-    system( "clear" )
-
-    print( "TODO list", len( tasks ), ":-)" )
-    
-    for i in range( len( tasks )):
-        
-        print( "\t", i + 1, ">", tasks[i] )
-
-    # ## print the tasks #######
 
 
 
